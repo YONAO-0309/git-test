c88cb0dfd4756d3c6835aea18d3fca99426fda8a
yoohoo <1569001613@qq.com>
2022-05-12 10:00:24 +0800

    first idea commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author yoohoo
+ * @Description: TODO
+ * @date 2022/5/12 9:56
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
